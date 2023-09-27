e062c4a2dbd4ba49544c6a1fcc09af8f9618458e
Tayyabar Rahman Khan <tayyabarrahmank@gmail.com>
2023-09-27 12:29:24 +0530

    Add files via upload
    This code will map over the expenses array and dynamically render an ExpenseItem component for each expense. The key prop is added to ensure React can efficiently update and re-render the components when needed. 
    This way, we can easily handle any number of expenses without manually writing ExpenseItem components for each one

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import ExpenseItem from './ExpenseItem'; // Import ExpenseItem component
 
 function App() {
   const expenses = [
     {
-      id: "e1",
-      title: "Toilet Paper",
+      id: 'e1',
+      title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2022, 2, 28),
-      locationOfExpenditure: "Grocery Store", // Add LocationOfExpenditure
+      locationOfExpenditure: 'Grocery Store'
     },
     {
-      id: "e2",
-      title: "New TV",
+      id: 'e2',
+      title: 'New TV',
       amount: 799.49,
       date: new Date(2022, 2, 29),
-      locationOfExpenditure: "Electronics Store", // Add LocationOfExpenditure
+      locationOfExpenditure: 'Electronics Store'
     },
     {
-      id: "e3",
-      title: "Car Insurance",
+      id: 'e3',
+      title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2022, 2, 30),
-      locationOfExpenditure: "Insurance Company", // Add LocationOfExpenditure
+      locationOfExpenditure: 'Insurance Company'
     },
+    // Add more expense objects here...
   ];
 
-  return <div>{/* Your ExpenseItem components rendering */}</div>;
+  return (
+    <div>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          locationOfExpenditure={expense.locationOfExpenditure}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
